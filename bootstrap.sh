a96a5a79d97f9e317f445ed088408962cc45e77a
James Green <james.green6@justice.gov.uk>
2024-06-13 13:13:14 +0100

    added in scripts to run, added tag that can be used by terraform to filter if we deploy to envs

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get update
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+sudo apt install unzip -y
+unzip /tmp/awscliv2.zip -d /tmp/
+sudo /tmp/aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:dns-oarc/dnsperf -y
+sudo apt install dnsperf -y
+sudo apt-get install dnsutils -y
+sudo apt-get install traceroute -y
+sudo snap install dnslookup
+
+sudo apt install mariadb-client-core-10.6 -y
